d12e9637f0a28fe223c1c114857f2c68498a517d
swc19 <swc19@snet.net>
2020-05-04 18:08:53 -0500

    Added zombie stun checks, need to implement actual stun

--- src/main/java/com/egsrit/hvz/listeners/StunListener.java
+++ src/main/java/com/egsrit/hvz/listeners/StunListener.java
@@ -4,7 +4,9 @@ import com.egsrit.hvz.players.Human;
 import com.egsrit.hvz.players.HvzZombie;
 import com.egsrit.hvz.util.Stats;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -17,17 +19,14 @@ public class StunListener implements Listener {
     // needs another event handler for seeing which weapon was used to stun the zombie (blaster, sock, elephant blaster)
 
     @EventHandler
-    public void onStun(EntityDamageByEntityEvent e){
+    public void onStunTag(EntityDamageByEntityEvent e){ // Witch/Twitch hits zombie
         if(e.getEntity() instanceof Player && e.getDamager() instanceof Player){
             Player entity = (Player) e.getEntity(); // the Zombie
-            Player damager = (Player) e.getDamager(); // the Human or Witch/Twitch
-            boolean damagerAlive = humanList.get(damager.getDisplayName()).isAlive(); // true if human, false if witch/twitch
-            String entitySpecialStatus = zombieList.get(entity.getDisplayName()).getSpecialStatus(); // used for special zombie stunning, null if human
+            Player damager = (Player) e.getDamager(); // the Witch/Twitch
+            String entitySpecialStatus = zombieList.get(entity.getDisplayName()).getSpecialStatus();
             String damagerSpecialStatus = zombieList.get(damager.getDisplayName()).getSpecialStatus();
-            if((damagerAlive && zombieList.get(entity.getDisplayName()) != null) ||
-                    ((damagerSpecialStatus.equals("Witch") || damagerSpecialStatus.equals("Twitch")) && zombieList.get(entity.getDisplayName()) != null)){
+            if((damagerSpecialStatus.equals("Witch") || damagerSpecialStatus.equals("Twitch")) && zombieList.get(entity.getDisplayName()) != null){
                 if(zombieList.get(entity.getDisplayName()).canBeStunned()){
-                    // check item used
                     stunZombie(entity, damager, entitySpecialStatus);
                     Stats.addStun(entity, damager);
                 }
@@ -37,18 +36,68 @@ public class StunListener implements Listener {
             }
         }
     }
-    public static void stunZombie(Player p, Player d, String special){
+
+    @EventHandler
+    public void onStunSnowball(EntityDamageByEntityEvent e){
+        if(e.getEntity() instanceof Player && e.getDamager() instanceof Snowball){
+            Player zombie = (Player) e.getEntity();
+            Snowball snowball = (Snowball) e.getDamager();
+            if(snowball.getShooter() instanceof Player) {
+                Player damager = (Player) snowball.getShooter();
+                if(humanList.get(damager.getDisplayName()).isAlive()){
+                    // All special zombies can be stunned by a snowball/sock
+                    if(zombieList.get(zombie.getDisplayName()).canBeStunned()){
+                        stunZombie(zombie, damager, null); // only humans will be in this action
+                        Stats.addStun(zombie, damager);
+                    }
+                    else {
+                        damager.sendMessage("You cannot stun this " + zombieList.get(zombie.getDisplayName()).getSpecialStatus() + " yet!");
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onStunArrow(EntityDamageByEntityEvent e){
+        if(e.getEntity() instanceof Player && e.getDamager() instanceof Arrow){
+            Player zombie = (Player) e.getEntity();
+            Arrow arrow = (Arrow) e.getDamager();
+            if(arrow.getShooter() instanceof Player){
+                Player damager = (Player) arrow.getShooter();
+                if(humanList.get(damager.getDisplayName()).isAlive()){
+                    if(zombieList.get(zombie.getDisplayName()).canBeStunned()){
+                        String specialStatus = zombieList.get(zombie.getDisplayName()).getSpecialStatus();
+                        switch(specialStatus){
+                            case "Tank":
+                            case "Twitch":
+                                damager.sendMessage("You cannot stun this " + specialStatus + " with a blaster!");
+                                break;
+                            default:
+                                stunZombie(zombie, damager, specialStatus);
+                        }
+                    }
+                    else {
+                        damager.sendMessage("You cannot stun this " + zombieList.get(zombie.getDisplayName()).getSpecialStatus() + " yet!");
+                    }
+                }
+            }
+        }
+
+    }
+    public static void stunZombie(Player zombie, Player damager, String special){
         ChatColor color;
         String message;
         if(special == (null)){ // human stunned zombie
-            color = humanList.get(d).getNameTagColor();
+            color = humanList.get(damager.getDisplayName()).getNameTagColor();
             message = "";
         }
-        else{
-            color = zombieList.get(d).getNameTagColor();
-            message = zombieList.get(d).getSpecialStatus() + " ";
+        else{ // witch/twitch stunned zombie
+            color = zombieList.get(damager.getDisplayName()).getNameTagColor();
+            message = zombieList.get(damager.getDisplayName()).getSpecialStatus() + " ";
         }
-        p.sendMessage("You've been stunned by " + color + message + d.getDisplayName() + "!");
+        zombieList.get(zombie.getDisplayName()).setStun(true);
+        zombie.sendMessage("You've been stunned by " + color + message + damager.getDisplayName() + "!");
         //use zombie's stun timer, make them unable to hit players/tag them
     }
 }
